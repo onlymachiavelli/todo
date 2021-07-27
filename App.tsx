6ac158492d4d637ddfa2d8dddb64b11ddc4307c6
Machiavelli <makaveli643@gmail.com>
2021-07-27 09:04:10 +0100

    Added Icons SVG

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import {  Text, View } from 'react-native'
+import {  Text, View, KeyboardAvoidingView, KeyboardAvoidingViewBase, TextInput } from 'react-native'
+import {Styles} from './Packs/styles'
+
+
 
 export default function App() {
   return (
-    <View>
-      <Text>SHEESH</Text>
-    </View>
+    <></>
   )
 }
--- Packs/styles.tsx
+++ Packs/styles.tsx
@@ -1,5 +1,9 @@
 import { StyleSheet} from "react-native"
 
-export const Styles = StyleSheet.create({
-    
+ export const Styles = StyleSheet.create({
+    root:{
+        width:"100%",
+        height:"100%",
+        backgroundColor:"#00ccff"
+    }  
 })
