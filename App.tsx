90fb78920156275d39d22ae258fdf72d97d09abe
Machiavelli <makaveli643@gmail.com>
2021-07-27 09:36:28 +0100

    Stacks

--- App.tsx
+++ App.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 import {  Text, View, KeyboardAvoidingView, KeyboardAvoidingViewBase, TextInput } from 'react-native'
 import {Styles} from './Packs/styles'
 import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, StackRouter } from '@react-navigation/native'
 const Pages = createStackNavigator
 
 export default function App() {
   return (
     <NavigationContainer>
-      
+      <Pages.Navigator>
+
+      </Pages.Navigator>
     </NavigationContainer>
   )
 }
