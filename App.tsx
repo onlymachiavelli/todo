d4004ccc9700b3c44b4f9c135feed94a98dc1d8e
Machiavelli <makaveli643@gmail.com>
2021-07-27 09:22:09 +0100

    Added Navigation

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import {  Text, View, KeyboardAvoidingView, KeyboardAvoidingViewBase, TextInput } from 'react-native'
 import {Styles} from './Packs/styles'
-
-
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+const Pages = createStackNavigator
 
 export default function App() {
   return (
-    <></>
+    <NavigationContainer>
+      
+    </NavigationContainer>
   )
 }
