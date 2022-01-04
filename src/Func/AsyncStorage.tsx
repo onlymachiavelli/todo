38156114c119450f00759155e6c36b579d7ddb7a
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-04 22:31:10 +0100

    async functions

---
+++ src/Func/AsyncStorage.tsx
@@ -0,0 +1,43 @@
+import { AsyncStorage } from "@react-native-async-storage/async-storage"
+
+
+export const storeDate = async(key:string,value:JSON) =>{
+    try{
+        await AsyncStorage.setItem (key, JSON.stringify(value))
+    }
+    catch(e) {
+        console.log(e)
+    }
+}
+
+export const getData =async (key:string) =>{
+    try{
+        return JSON.parse(await AsyncStorage.getItem(key))
+    }
+    catch(e){
+        console.log(e)
+    }
+
+}
+
+export const deleteItem = async (key:string) =>{
+    try {
+        await AsyncStorage.removeItem(key)
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+
+export const clearStorage = async () =>{
+    try{
+        await AsyncStorage.clear()
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+
+
+
---
+++ src/UI/Header.tsx
@@ -0,0 +1,9 @@
+import React , {} from 'react' 
+import {} from './Style'
+import { View} from 'react-native'
+
+const Header = () =>{
+    return (
+        <View></View>
+    )
+}
