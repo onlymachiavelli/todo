bdb3ab7d4661065f19865b44c2ac55db1252100f
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-05 07:25:26 +0100

    pressing btn

--- src/UI/Header.tsx
+++ src/UI/Header.tsx
@@ -1,13 +1,24 @@
-import React , {} from 'react' 
+import React , {useState, useEffect} from 'react' 
 import {headerStyle} from './Style'
-import { View, Text} from 'react-native'
+import { View, Text, TouchableOpacity} from 'react-native'
 import {Rocket} from './SVG'
+export const headerVisibility = () =>{
+    const [visible, setVis] = useState(false)
+    useEffect(()=>{
+
+    },[])
+}
 const Header = () =>{
     return (
         <View style={headerStyle.container}>
             <View>
                 <Rocket Width="59" Height="67" /> 
                 <Text style={ headerStyle.textLogo}>TODO APP</Text>
+            </View>
+            <View style={headerStyle.buttonBlock}>
+                <TouchableOpacity style={headerStyle.Button} onPress={()=>{}}>
+                    
+                </TouchableOpacity>
             </View>
         </View>
     )
