a2a8524f06d3a008a55243790ded4612abddf745
onlymachiavelli <tmakaveli643@gmail.com>
2022-01-08 18:04:24 +0100

    fake

--- src/UI/myTasks.tsx
+++ src/UI/myTasks.tsx
@@ -3,6 +3,33 @@ import {View, Text} from 'react-native'
 import {} from './SVG'
 import {} from './Style'
 const MyTasks = ({...props}) =>{
+    const Datas = [
+        {
+            taskTitle:"fucking your bitch",
+            Details:"Im going to fuck your bitch until you die",
+            isDone:false
+        },
+        {
+            taskTitle:"fucking your bitch",
+            Details:"Im going to fuck your bitch until you die",
+            isDone:true
+        },
+        {
+            taskTitle:"fucking your bitch",
+            Details:"Im going to fuck your bitch until you die",
+            isDone:false
+        },
+        {
+            taskTitle:"fucking your bitch",
+            Details:"Im going to fuck your bitch until you die",
+            isDone:false
+        },
+        {
+            taskTitle:"fucking your bitch",
+            Details:"Im going to fuck your bitch until you die",
+            isDone:true
+        },
+    ]
     return (
         <View></View>
     )
